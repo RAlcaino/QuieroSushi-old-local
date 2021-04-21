83b5923e8356e8b011b2328ef61c8f43ae74d748
Gabriel <garomerol.17@est.ucab.edu.ve>
2021-04-22 01:04:47 +0200

    feat: api route

--- src/store/modules/routes/routes.js
+++ src/store/modules/routes/routes.js
@@ -1,7 +1,7 @@
 const state = {
-  //urlGlobal: "https://api.panelnuevo.quierosushi.cl/api",
+  urlGlobal: "https://api.panelnuevo.quierosushi.cl/api",
   //urlGlobal: "http://127.0.0.1:8000/api",
-  urlGlobal: "http://190.79.241.72:5000/api/public/api",
+  //urlGlobal: "http://190.79.241.72:5000/api/public/api",
   routes: [
     {
       name: "login",
