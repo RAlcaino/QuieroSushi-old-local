258fe60270008afae8973a3a3910febb159a7492
Gabriel Romero <garomerol.17@est.ucab.edu.ve>
2021-08-10 22:35:51 -0400

    feat: mini chat

--- src/components/modals/ModalStatusOrder.vue
+++ src/components/modals/ModalStatusOrder.vue
@@ -2,7 +2,7 @@
   <q-dialog v-model="open" persistent>
     <q-card
       class="my-card"
-      style="border-radius:10px;width: 500px; max-width: 80vw; overflow:hidden"
+      style="border-radius:10px;width: 600px; max-width: 80vw; overflow:hidden"
     >
       <q-card-section
         class="row items-center q-pb-none"
@@ -18,14 +18,17 @@
         </div>
       </q-card-section>
 
-      <q-card-section v-if="data !== null">
+      <q-card-section
+        v-if="data !== null"
+        style="height: 450px; overflow-y: auto;"
+      >
         <q-list>
           <q-item>
             <q-item-section avatar top>
               <q-icon name="message" color="primary" />
             </q-item-section>
             <q-item-section>
-              <q-item-label><strong>Mensaje</strong></q-item-label>
+              <q-item-label><strong>Último Mensaje</strong></q-item-label>
               <q-item-label>{{ data.mensaje }}</q-item-label>
             </q-item-section>
           </q-item>
@@ -34,59 +37,52 @@
               <q-icon name="help_center" color="primary" />
             </q-item-section>
             <q-item-section>
-              <q-item-label><strong>Estado</strong></q-item-label>
-              <q-option-group
-                :options="options"
-                type="radio"
-                v-model="group"
-                @input="changeStatus()"
-              />
+              <q-item-label><strong>Respuesta</strong></q-item-label>
+              <div class="q-pa-md" style="display: flex;">
+                <q-input
+                  v-model="text"
+                  dense
+                  outlined
+                  rounded
+                  autogrow
+                  style="width:80%;margin-right:5px;"
+                />
+                <q-btn
+                  color="green"
+                  round
+                  :disable="validation"
+                  size="sm"
+                  @click="notify()"
+                  style="width: 35px; height:35px; margin-top: 2px;"
+                >
+                  <q-icon size="20px" name="send" />
+                </q-btn>
+              </div>
             </q-item-section>
           </q-item>
-          <q-item style="margin-top: 10px;" v-if="group === 'delay'">
+          <q-item style="margin-top: 10px;">
             <q-item-section avatar top>
-              <q-icon name="schedule" color="primary" />
+              <q-icon name="question_answer" color="primary" />
             </q-item-section>
             <q-item-section>
-              <q-item-label
-                ><strong>¿A que hora aproximadamente?</strong></q-item-label
-              >
-              <q-input
-                v-model="time"
-                mask="time"
-                :rules="['time']"
-                dense
-                outlined
-                rounded
-                label="Hora"
-                style="margin-top: 10px; width:50%;"
-              >
-                <template v-slot:append>
-                  <q-icon name="access_time" class="cursor-pointer">
-                    <q-popup-proxy
-                      transition-show="scale"
-                      transition-hide="scale"
-                    >
-                      <q-time v-model="time" format24h>
-                        <div class="row items-center justify-end">
-                          <q-btn
-                            v-close-popup
-                            label="Cerrar"
-                            color="primary"
-                            flat
-                          />
-                        </div>
-                      </q-time>
-                    </q-popup-proxy>
-                  </q-icon>
-                </template>
-              </q-input>
+              <q-item-label><strong>Chat</strong></q-item-label>
+              <div style="width: 100%; max-width: 400px">
+                <q-chat-message
+                  v-for="msg in messages"
+                  :key="msg.id"
+                  :name="msg.origen"
+                  :avatar="avatarMsg(msg.origen)"
+                  :text="[msg.mensaje]"
+                  :sent="msg.origen === 'Local' ? true : false"
+                  :stamp="msg.timestamp"
+                />
+              </div>
             </q-item-section>
           </q-item>
         </q-list>
       </q-card-section>
 
-      <q-card-actions align="right" style="height: 20%;">
+      <!--<q-card-actions align="right" style="height: 20%;">
         <q-btn
           rounded
           :disable="validation"
@@ -95,7 +91,7 @@
           style="font-size: 11px !important"
           @click="notify()"
         />
-      </q-card-actions>
+      </q-card-actions>-->
     </q-card>
   </q-dialog>
 </template>
@@ -105,11 +101,10 @@ export default {
   inject: ["showNotification", "showLoading", "hideLoading", "errorHandling"],
   computed: {
     validation() {
-      if (this.group === null) {
+      if (this.text === null || this.text.trim().length === 0) {
         return true;
-      }
-      if (this.group === "delay" && this.time === null) {
-        return true;
+      } else {
+        return false;
       }
     }
   },
@@ -117,38 +112,36 @@ export default {
     return {
       open: false,
       data: null,
-      options: [
-        { label: "Ya esta en camino", value: "on-the-way", color: "green" },
-        { label: "Ya esta por salir", value: "almost-ready", color: "blue" },
+      text: null,
+      messages: [
         {
-          label: "Llegara mas tarde de lo acordado",
-          value: "delay",
-          color: "red"
-        }
-      ],
-      group: null,
-      time: null
+          id: 6,
+          id_venta: 2,
+          origen: "SAC",
+          mensaje: "Que pasa con la orden?",
+          timestamp: "2021-08-10 19:45:12"
+        },
+        {
+          id: 7,
+          id_venta: 2,
+          origen: "Local",
+          mensaje: "Salio hace 20 minutos",
+          timestamp: "2021-08-10 19:45:12"
+        },
+      ]
     };
   },
   created() {
     this.bus.$on("modal-status-order-?", data => {
       this.open = true;
       this.data = { ...data };
-      console.log(data);
     });
   },
   methods: {
     notify() {
-      let msg = "";
-      let each = this.options.map(el => {
-        if (el.value === this.group) {
-          msg = el.label;
-        }
-      });
       var data = {
-        orderId: this.data.id_venta,
-        message:
-          this.group === "delay" ? `${msg} ${this.time}.` : msg
+        orderId: +this.data.id_venta,
+        message: this.text
       };
       var url = this.$store.getters["routes/getRoute"](
         "notification.status.order"
@@ -174,6 +167,9 @@ export default {
       if (this.group !== "delay") {
         this.time = null;
       }
+    },
+    avatarMsg(origen) {
+      return origen === "Local" ? "img/store.png" : "img/sca.png";
     }
   }
 };
